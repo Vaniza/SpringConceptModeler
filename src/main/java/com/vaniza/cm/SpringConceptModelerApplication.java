b2054a524e56db1e77a4cad5f881eac30094830e
Vaniza <vaniza@outlook.com>
2024-01-17 17:01:47 -0300

    First commit

---
+++ src/main/java/com/vaniza/cm/SpringConceptModelerApplication.java
@@ -0,0 +1,13 @@
+package com.vaniza.cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptModelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptModelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaniza/cm/SpringConceptModelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaniza.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConceptModelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
